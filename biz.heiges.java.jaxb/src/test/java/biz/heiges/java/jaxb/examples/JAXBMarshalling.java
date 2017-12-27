73604b1ffe580c76df7e92ab785ed9f12cef73a3
Hansjoachim Heiges <hansjoachim.heiges@heiges.biz>
2017-12-27 17:47:28 +0100

    added testcases for required but absent fields

--- biz.heiges.java.jaxb/src/test/java/biz/heiges/java/jaxb/examples/JAXBMarshalling.java
+++ biz.heiges.java.jaxb/src/test/java/biz/heiges/java/jaxb/examples/JAXBMarshalling.java
@@ -1,6 +1,7 @@
 package biz.heiges.java.jaxb.examples;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
 import java.io.File;
@@ -36,6 +37,26 @@ public class JAXBMarshalling {
 	}
 	
 	@Test
+	public void testUnmashallingWithRequiredFieldMissing()  {
+
+		try {
+			JAXBContext ctx = JAXBContext.newInstance(new Class[] { Person.class, Address.class });
+			Unmarshaller unmarshaller = ctx.createUnmarshaller();
+			Person p2 = (Person) unmarshaller.unmarshal(new FileReader("testcases/person4unmarshallingWithRequiredFieldMissing.xml"));
+			// Unmrshalling will work even when required fields are missing
+			assertNull(p2.getFamilieName());
+			assertEquals("Hansjoachim", p2.getSurName());
+			assertEquals("Hajo", p2.getNickName());
+			assertEquals("München", p2.getAddress().getCity());
+			assertEquals("Rotenhanstrasse 6", p2.getAddress().getStreet());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	
+	@Test
 	public void testMashalling()  {
 		try {
 			JAXBContext ctx = JAXBContext.newInstance(new Class[] { Person.class, Address.class });
@@ -51,6 +72,29 @@ public class JAXBMarshalling {
 			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 			marshaller.setProperty(Marshaller.JAXB_ENCODING, "ISO-8859-1" );
 			marshaller.marshal(p1, new File("temp/person.xml"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Test
+	public void testMashallingWithRequiredFieldMissing()  {
+		try {
+			JAXBContext ctx = JAXBContext.newInstance(new Class[] { Person.class, Address.class });
+			Person p1 = new Person();
+			// familieName is required but missing!
+			//p1.setFamilieName("Heiges");
+			p1.setSurName("Hansjoachim");
+			p1.setNickName("Hajo");
+			Address a1 = new Address();
+			a1.setCity("München");
+			a1.setStreet("Rotenhanstrasse 6");
+			p1.setAddress(a1);
+			Marshaller marshaller = ctx.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			marshaller.setProperty(Marshaller.JAXB_ENCODING, "ISO-8859-1" );
+			marshaller.marshal(p1, new File("temp/personWithRequiredFieldMissing.xml"));
 		} catch (Exception e) {
 			e.printStackTrace();
 			fail();
