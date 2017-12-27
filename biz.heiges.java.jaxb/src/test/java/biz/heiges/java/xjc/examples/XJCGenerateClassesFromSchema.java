d2e011fcad14f48b46363c5b5375df6d6bf16bf8
Hansjoachim Heiges <hansjoachim.heiges@heiges.biz>
2017-12-27 22:45:42 +0100

    fixed dependencies for xjc

--- biz.heiges.java.jaxb/src/test/java/biz/heiges/java/xjc/examples/XJCGenerateClassesFromSchema.java
+++ biz.heiges.java.jaxb/src/test/java/biz/heiges/java/xjc/examples/XJCGenerateClassesFromSchema.java
@@ -8,27 +8,27 @@ import java.io.IOException;
 import org.junit.Test;
 import org.xml.sax.InputSource;
 
-//import com.sun.codemodel.JCodeModel;
-//import com.sun.tools.xjc.api.S2JJAXBModel;
-//import com.sun.tools.xjc.api.SchemaCompiler;
-//import com.sun.tools.xjc.api.XJC;
+import com.sun.codemodel.JCodeModel;
+import com.sun.tools.xjc.api.S2JJAXBModel;
+import com.sun.tools.xjc.api.SchemaCompiler;
+import com.sun.tools.xjc.api.XJC;
 
 public class XJCGenerateClassesFromSchema {
 
-//	@Test
-//	public void test() {
-//        SchemaCompiler sc = XJC.createSchemaCompiler();
-//        sc.forcePackageName("temp.biz.heiges.generated");
-//        File schemaFile = new File("testcases/person.xsd");
-//        InputSource is = new InputSource(schemaFile.toURI().toString());
-//        sc.parseSchema(is);
-//        S2JJAXBModel model = sc.bind();
-//        JCodeModel jCodeModel = model.generateCode(null, null);
-//        try {
-//			jCodeModel.build(new File("."));
-//		} catch (IOException e) {
-//			fail();
-//		}
-//	}
+	@Test
+	public void test() {
+        SchemaCompiler sc = XJC.createSchemaCompiler();
+        sc.forcePackageName("temp.biz.heiges.generated");
+        File schemaFile = new File("testcases/person.xsd");
+        InputSource is = new InputSource(schemaFile.toURI().toString());
+        sc.parseSchema(is);
+        S2JJAXBModel model = sc.bind();
+        JCodeModel jCodeModel = model.generateCode(null, null);
+        try {
+			jCodeModel.build(new File("."));
+		} catch (IOException e) {
+			fail();
+		}
+	}
 
 }
