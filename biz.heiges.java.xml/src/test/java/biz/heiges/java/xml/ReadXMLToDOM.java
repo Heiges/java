98314ed91a71ec861e870b1effe56898dc24997c
Hansjoachim Heiges <hansjoachim.heiges@heiges.biz>
2017-12-28 14:41:26 +0100

    first testcase round 1

--- biz.heiges.java.xml/src/test/java/biz/heiges/java/xml/ReadXMLToDOM.java
+++ biz.heiges.java.xml/src/test/java/biz/heiges/java/xml/ReadXMLToDOM.java
@@ -1,19 +1,35 @@
 package biz.heiges.java.xml;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import org.junit.Before;
+import java.io.FileReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
 import org.junit.Test;
+
+import biz.heiges.java.model.Address;
+import biz.heiges.java.model.Person;
 
 public class ReadXMLToDOM {
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testUnmashalling() {
+		try {
+			JAXBContext ctx = JAXBContext.newInstance(new Class[] { Person.class, Address.class });
+			Unmarshaller unmarshaller = ctx.createUnmarshaller();
+			Person p2 = (Person) unmarshaller.unmarshal(new FileReader("testcases/person.xml"));
+			assertEquals("Heiges", p2.getFamilieName());
+			assertEquals("Hansjoachim", p2.getSurName());
+			assertEquals("Hajo", p2.getNickName());
+			assertEquals("München", p2.getAddress().getCity());
+			assertEquals("Rotenhanstrasse 6", p2.getAddress().getStreet());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 
 }
