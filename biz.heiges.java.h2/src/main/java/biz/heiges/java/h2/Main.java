0ad45852319ac44a1f3b52acfebdd7a5f6c80339
Heiges <hansjoachim.heiges@heiges.biz>
2020-11-30 21:16:17 +0100

    added missing crud operations

--- biz.heiges.java.h2/src/main/java/biz/heiges/java/h2/Main.java
+++ biz.heiges.java.h2/src/main/java/biz/heiges/java/h2/Main.java
@@ -18,8 +18,32 @@ public class Main {
 			em.persist(new PersonDAO("Micky", "Mouse"));
 			t.commit();
 			
+			System.out.println("*** get all persons");
 			List<PersonDAO> resultList = em.createNativeQuery("SELECT * FROM PERSON", PersonDAO.class).getResultList();
 			resultList.forEach((x) -> System.out.printf("- %s - %s %s %n", x.getId(), x.getSurname(), x.getFamilyname()));
+			
+			System.out.println("*** read person with id 3");
+			t.begin();
+			PersonDAO p = em.find(PersonDAO.class, Long.parseLong("3"));
+			System.out.printf("- %s - %s %s %n", p.getId(), p.getSurname(), p.getFamilyname());
+			t.commit();
+			
+			System.out.println("*** update person with id 3");
+			t.begin();
+			p.setSurname("Mini");
+			p.setFamilyname("Mouse");
+			em.merge(p);
+			t.commit();
+			resultList = em.createNativeQuery("SELECT * FROM PERSON", PersonDAO.class).getResultList();
+			resultList.forEach((x) -> System.out.printf("- %s - %s %s %n", x.getId(), x.getSurname(), x.getFamilyname()));
+			
+			System.out.println("*** delete person with id 3");
+			t.begin();
+			em.remove(em.find(PersonDAO.class, Long.parseLong("3")));
+			t.commit();
+			resultList = em.createNativeQuery("SELECT * FROM PERSON", PersonDAO.class).getResultList();
+			resultList.forEach((x) -> System.out.printf("- %s - %s %s %n", x.getId(), x.getSurname(), x.getFamilyname()));
+			
 		}
 	}
 }
