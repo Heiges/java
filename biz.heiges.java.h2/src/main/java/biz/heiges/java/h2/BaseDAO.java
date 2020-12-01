5f41f08e93ec393edf7cf5c3071259e43a65ca78
Heiges <hansjoachim.heiges@heiges.biz>
2020-12-01 22:27:14 +0100

    added helperclass BaseDAO

---
+++ biz.heiges.java.h2/src/main/java/biz/heiges/java/h2/BaseDAO.java
@@ -0,0 +1,72 @@
+package biz.heiges.java.h2;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class BaseDAO<T extends Serializable> implements AutoCloseable {
+
+	private Class< T > clazz = null;
+
+	private EntityManagerFactory emf = null;
+
+	private EntityManager em = null;
+	
+	private EntityTransaction transaction = null;	
+	
+	public BaseDAO(String persistenceUnitName) {
+		emf = Persistence.createEntityManagerFactory(persistenceUnitName);
+		em= emf.createEntityManager();
+	}
+	
+	public void create(T entity) {
+		begin();
+		em.persist(new PersonDAO("Micky", "Mouse"));
+		transaction.commit();
+	}
+	
+	public List<PersonDAO> read() {
+		return em.createNativeQuery("SELECT * FROM PERSON", clazz).getResultList();
+	}
+
+	public PersonDAO read(Long primaryKey) {
+		return (PersonDAO) em.find(clazz, primaryKey);
+	}
+
+	public void update(PersonDAO entity) {
+		begin();
+		em.merge(entity);
+		transaction.commit();
+	}
+
+	public void delete(PersonDAO entity) {
+		begin();
+		em.remove(entity);
+		transaction.commit();
+	}
+
+	private void begin() {
+		validateClazz();
+		transaction = em.getTransaction();
+		transaction.begin();
+	}
+	
+	private void validateClazz() {
+		if (clazz == null) throw new IllegalArgumentException("now clazz given");
+	}
+	
+	public void setClazz(Class< T > clazz) {
+		this.clazz = clazz;
+	}
+	
+	@Override
+	public void close() throws Exception {
+		System.out.println("calling close()");
+		if (em.isOpen() == true) em.close();
+		if(emf.isOpen() == true) emf.close();
+	}
+}
--- biz.heiges.java.h2/src/main/java/biz/heiges/java/h2/Main.java
+++ biz.heiges.java.h2/src/main/java/biz/heiges/java/h2/Main.java
@@ -2,48 +2,33 @@ package biz.heiges.java.h2;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-
 public class Main {
 
 	public static void main(String... args) throws Exception {
 	
-		// via JPA
-		try (EntityManagerPool emp = new EntityManagerPool("h2")) {
-			EntityManager em = emp.getEntityManager();
+		try (BaseDAO<PersonDAO> personDAO = new BaseDAO<PersonDAO>("h2")) {
+			personDAO.setClazz(PersonDAO.class);
+			personDAO.create(new PersonDAO("Micky", "Mouse"));
 
-			EntityTransaction t = em.getTransaction();
-			t.begin();
-			em.persist(new PersonDAO("Micky", "Mouse"));
-			t.commit();
-			
 			System.out.println("*** get all persons");
-			List<PersonDAO> resultList = em.createNativeQuery("SELECT * FROM PERSON", PersonDAO.class).getResultList();
+			List<PersonDAO> resultList = personDAO.read();
 			resultList.forEach((x) -> System.out.printf("- %s - %s %s %n", x.getId(), x.getSurname(), x.getFamilyname()));
 			
 			System.out.println("*** read person with id 3");
-			t.begin();
-			PersonDAO p = em.find(PersonDAO.class, Long.parseLong("3"));
+			PersonDAO p = personDAO.read(Long.parseLong("3"));
 			System.out.printf("- %s - %s %s %n", p.getId(), p.getSurname(), p.getFamilyname());
-			t.commit();
 			
 			System.out.println("*** update person with id 3");
-			t.begin();
 			p.setSurname("Mini");
 			p.setFamilyname("Mouse");
-			em.merge(p);
-			t.commit();
-			resultList = em.createNativeQuery("SELECT * FROM PERSON", PersonDAO.class).getResultList();
+			personDAO.update(p);
+			resultList = personDAO.read();
 			resultList.forEach((x) -> System.out.printf("- %s - %s %s %n", x.getId(), x.getSurname(), x.getFamilyname()));
 			
 			System.out.println("*** delete person with id 3");
-			t.begin();
-			em.remove(em.find(PersonDAO.class, Long.parseLong("3")));
-			t.commit();
-			resultList = em.createNativeQuery("SELECT * FROM PERSON", PersonDAO.class).getResultList();
+			personDAO.delete(personDAO.read(Long.parseLong("3")));
+			resultList = personDAO.read();
 			resultList.forEach((x) -> System.out.printf("- %s - %s %s %n", x.getId(), x.getSurname(), x.getFamilyname()));
-			
 		}
 	}
 }
