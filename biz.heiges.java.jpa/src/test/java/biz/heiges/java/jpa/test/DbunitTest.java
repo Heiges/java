9e822f015baaedf62ed935cb42fec743006978c5
Heiges <hansjoachim.heiges@heiges.biz>
2020-12-10 22:43:02 +0100

    added some testcases utilizing dbunit

--- biz.heiges.java.jpa/src/test/java/biz/heiges/java/jpa/test/DbunitTest.java
+++ biz.heiges.java.jpa/src/test/java/biz/heiges/java/jpa/test/DbunitTest.java
@@ -1,45 +1,88 @@
 package biz.heiges.java.jpa.test;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
-import org.dbunit.IDatabaseTester;
-import org.dbunit.JdbcDatabaseTester;
+import javax.sql.DataSource;
+
+import org.dbunit.Assertion;
+import org.dbunit.DataSourceBasedDBTestCase;
+import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
-import org.dbunit.operation.DatabaseOperation;
-import org.h2.tools.RunScript;
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.Test;
 
 import biz.heiges.java.jpa.BaseDAO;
 import biz.heiges.java.jpa.Database;
 import biz.heiges.java.jpa.test.entities.ParentEntityDAO;
 
-public class DbunitTest {
-	
-	private static final String JDBC_DRIVER = org.h2.Driver.class.getName();
-	private static final String JDBC_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
-	private static final String USER = "sa";
-	private static final String PASSWORD = "";
+public class DbunitTest extends DataSourceBasedDBTestCase  {
 	
 	@Test
-	public void test() throws Exception {
+	public void testDatabaseHasBeenSetup() throws Exception {
 		
-		RunScript.execute("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "", new File(this.getClass().getResource("/sql/create.sql").toURI()).toString(), null, false);
-
-		IDataSet dataSet = new FlatXmlDataSetBuilder().build(new File(this.getClass().getResource("/dbunit/dataset.xml").toURI()));
-
-		IDatabaseTester databaseTester = new JdbcDatabaseTester(JDBC_DRIVER, JDBC_URL, USER, PASSWORD);
-		databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
-		databaseTester.setDataSet(dataSet);
-		databaseTester.onSetup();
-
 		try (Database databaseWithoutCreate = new Database("entitiesWithoutCreate")) {
 			BaseDAO<ParentEntityDAO> dao = new BaseDAO<ParentEntityDAO>();
 			dao.setClazz(ParentEntityDAO.class);
 			dao.setEntityManager(databaseWithoutCreate.getEntityManager());
-			assertEquals("aValueForEntityWithID2_andDBUnit", dao.read(Long.parseLong("3")).getaSimpleCharValue());
-		}			
+			assertEquals("aValueForEntityWithID0_andDBUnit", dao.read(Long.parseLong("0")).getaSimpleCharValue());
+			assertEquals("aValueForEntityWithID1_andDBUnit", dao.read(Long.parseLong("1")).getaSimpleCharValue());
+			assertEquals("aValueForEntityWithID2_andDBUnit", dao.read(Long.parseLong("2")).getaSimpleCharValue());
+		}		
+		
+        IDataSet databaseDataSet = getConnection().createDataSet();
+        ITable actualTable = databaseDataSet.getTable("PARENT_ENTITIES");
+
+		FlatXmlDataSet expectedData = readDataset("/dbunit/expected_dataset.xml");
+		ITable expectedTable = expectedData.getTable("PARENT_ENTITIES");
+		
+		Assertion.assertEquals(expectedTable, actualTable);
 	}
+
+	@Test
+	public void testDatabaseAfterAddingARow() throws Exception {
+		
+		try (Database databaseWithoutCreate = new Database("entitiesWithoutCreate")) {
+			BaseDAO<ParentEntityDAO> dao = new BaseDAO<ParentEntityDAO>();
+			dao.setClazz(ParentEntityDAO.class);
+			dao.setEntityManager(databaseWithoutCreate.getEntityManager());
+			dao.create(new ParentEntityDAO("another Value"));
+
+			assertEquals("aValueForEntityWithID0_andDBUnit", dao.read(Long.parseLong("0")).getaSimpleCharValue());
+			assertEquals("aValueForEntityWithID1_andDBUnit", dao.read(Long.parseLong("1")).getaSimpleCharValue());
+			assertEquals("aValueForEntityWithID2_andDBUnit", dao.read(Long.parseLong("2")).getaSimpleCharValue());
+			assertEquals("another Value", dao.read(Long.parseLong("3")).getaSimpleCharValue());
+			assertNull(dao.read(Long.parseLong("4")));
+		}
+		
+        IDataSet databaseDataSet = getConnection().createDataSet();
+        ITable actualTable = databaseDataSet.getTable("PARENT_ENTITIES");
+
+		FlatXmlDataSet expectedData = readDataset("/dbunit/expected_dataset_afterCreate.xml");
+		ITable expectedTable = expectedData.getTable("PARENT_ENTITIES");
+		 
+		Assertion.assertEquals(expectedTable, actualTable);
+	}
+	
+	private FlatXmlDataSet readDataset(String initialData) throws MalformedURLException, DataSetException, URISyntaxException, ClassNotFoundException, Exception {
+		return new FlatXmlDataSetBuilder().build(new File(this.getClass().getResource(initialData).toURI()));
+	}
+
+	@Override
+	protected DataSource getDataSource() {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;init=runscript from 'classpath:/dbunit/create.sql'");
+        dataSource.setUser("sa");
+        dataSource.setPassword("");
+        return dataSource;
+	}
+
+	@Override
+	protected IDataSet getDataSet() throws Exception {
+		return readDataset("/dbunit/dataset.xml");
+	}	
 }
